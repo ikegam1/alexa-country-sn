fb515ec7aad74b410659f6ad502778bc70ff7797
masrao <takesiba.masrao@gmail.com>
2018-11-07 00:00:06 +0900

    release

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from chalice import Chalice
-<<<<<<< HEAD
 import logging
 import json
 import random
@@ -147,12 +146,9 @@ def quizIntent(session):
     logger.info(str(quiz))
     
     text += quiz["q"]
-    text += '<break time="0.5s" /> %s' % quiz["q"]
-    text += '<break time="10s"/>あと10秒です。<break time="7s"/>さん。<break time="1s"/>にい。<break time="1s"/>いち。<break time="1s"/>'
+    text += '<break time="1s" /> %s ' % quiz["q"]
+    text2 = 'あと10秒です。<break time="6s"/>さん。<break time="1s"/>にい。<break time="1s"/>いち。<break time="0.5s"/>どうぞ'
 
-    text2 = 'もう一度だけ問題を言いますよ。'
-    text2 += quiz["q"]
-    text2 += '<break time="1s"/>さん。<break time="1s"/>にい。<break time="1s"/>いち。<break time="1s"/>'
     logger.info(text)
     logger.info(text2)
 
@@ -211,35 +207,3 @@ def fallback(session_attribute={}):
     ssml.append('ハッキリと聞き取れなかったです。もう一度言ってみて。')
     return QuestionSpeech(random.choice(ssml),session_attribute).build()
 
-
-
-=======
-
-app = Chalice(app_name='alexa-country-sn')
-
-
-@app.route('/')
-def index():
-    return {'hello': 'world'}
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
->>>>>>> origin/master
